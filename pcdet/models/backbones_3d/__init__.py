0c7f61ddf71551a41e36a12fa0516a7d19fbaf15
DeclK <1605224559@qq.com>
2022-07-08 19:05:54 +0800

    add spconv focal lidar scripts

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -4,6 +4,7 @@ from .spconv_unet import UNetV2
 from .aux_backbone import AuxVoxelBackBone8x
 from .SpMiddleFHD import SpMiddleFHD
 from .pillar_encoder import SpMiddlePillarEncoder
+from .spconv_focal import VoxelBackBone8xFocal
 
 __all__ = {
     'VoxelBackBone8x': VoxelBackBone8x,
@@ -14,4 +15,5 @@ __all__ = {
     'AuxVoxelBackBone8x': AuxVoxelBackBone8x,
     'SpMiddleFHD': SpMiddleFHD,
     'SpMiddlePillarEncoder': SpMiddlePillarEncoder,
+    'VoxelBackBone8xFocal': VoxelBackBone8xFocal,
 }
